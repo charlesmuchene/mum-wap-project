febe3c17561dd69d982c85ee650260f3d3b1b96a
Haile Berhe Weldu <haile@Hailes-MacBook-Pro.local>
2019-02-05 22:11:38 -0600

    Add user and add team forms created

--- src/main/webapp/resources/scripts/tasks-controller.js
+++ src/main/webapp/resources/scripts/tasks-controller.js
@@ -1,47 +1,48 @@
-tasksController = function() { 
-	
+
+tasksController = function() {
+
 	function errorLogger(errorCode, errorMessage) {
 		console.log(errorCode +':'+ errorMessage);
 	}
-	
+
 	var taskPage;
 	var initialised = false;
 
-    /**
+	/**
 	 * makes json call to server to get task list.
 	 * currently just testing this and writing return value out to console
 	 * 111917kl
-     */
+	 */
 	function retrieveTasksServer() {
-        $.ajax("TaskServlet", {
-            "type": "get",
+		$.ajax("TaskServlet", {
+			"type": "get",
 			dataType: "json"
-            // "data": {
-            //     "first": first,
-            //     "last": last
-            // }
-        }).done(displayTasksServer.bind()); //need reference to the tasksController object
-    }
+			// "data": {
+			//     "first": first,
+			//     "last": last
+			// }
+		}).done(displayTasksServer.bind()); //need reference to the tasksController object
+	}
 
-    /**
+	/**
 	 * 111917kl
 	 * callback for retrieveTasksServer
-     * @param data
-     */
-    function displayTasksServer(data) { //this needs to be bound to the tasksController -- used bind in retrieveTasksServer 111917kl
-    	console.log(data);
-        tasksController.loadServerTasks(data);
-    }
-	
+	 * @param data
+	 */
+	function displayTasksServer(data) { //this needs to be bound to the tasksController -- used bind in retrieveTasksServer 111917kl
+		console.log(data);
+		tasksController.loadServerTasks(data);
+	}
+
 	function taskCountChanged() {
 		var count = $(taskPage).find( '#tblTasks tbody tr').length;
 		$('footer').find('#taskCount').text(count);
 	}
-	
+
 	function clearTask() {
 		$(taskPage).find('form').fromObject({});
 	}
-	
+
 	function renderTable() {
 		$.each($(taskPage).find('#tblTasks tbody tr'), function(idx, row) {
 			var due = Date.parse($(row).find('[datetime]').text());
@@ -52,9 +53,9 @@ tasksController = function() {
 			}
 		});
 	}
-	
-	return { 
-		init : function(page, callback) { 
+
+	return {
+		init : function(page, callback) {
 			if (initialised) {
 				callback()
 			} else {
@@ -62,14 +63,24 @@ tasksController = function() {
 				storageEngine.init(function() {
 					storageEngine.initObjectStore('task', function() {
 						callback();
-					}, errorLogger) 
-				}, errorLogger);	 				
+					}, errorLogger)
+				}, errorLogger);
 				$(taskPage).find('[required="required"]').prev('label').append( '<span>*</span>').children( 'span').addClass('required');
 				$(taskPage).find('tbody tr:even').addClass('even');
-				
+
 				$(taskPage).find('#btnAddTask').click(function(evt) {
 					evt.preventDefault();
 					$(taskPage).find('#taskCreation').removeClass('not');
+				});
+
+				$(taskPage).find('#addTeamButton').click(function(evt) {
+					evt.preventDefault();
+					$(taskPage).find('.teamWrapper').removeClass('not');
+				});
+
+				$(taskPage).find('#addUserButton').click(function(evt) {
+					evt.preventDefault();
+					$(taskPage).find('.userWrapper').removeClass('not');
 				});
 
 				$(taskPage).find('#addUserButton').click(function(evt) {
@@ -77,43 +88,43 @@ tasksController = function() {
 					$(taskPage).find('#userCreation').removeClass('not');
 				});
 
-                /**	 * 11/19/17kl        */
-                $(taskPage).find('#btnRetrieveTasks').click(function(evt) {
-                    evt.preventDefault();
-                    console.log('making ajax call');
-                    retrieveTasksServer();
-                });
-				
+				/**     * 11/19/17kl        */
+				$(taskPage).find('#btnRetrieveTasks').click(function(evt) {
+					evt.preventDefault();
+					console.log('making ajax call');
+					retrieveTasksServer();
+				});
+
 				$(taskPage).find('#tblTasks tbody').on('click', 'tr', function(evt) {
 					$(evt.target).closest('td').siblings().andSelf().toggleClass('rowHighlight');
-				});	
-				
-				$(taskPage).find('#tblTasks tbody').on('click', '.deleteRow', 
-					function(evt) { 					
-						storageEngine.delete('task', $(evt.target).data().taskId, 
+				});
+
+				$(taskPage).find('#tblTasks tbody').on('click', '.deleteRow',
+					function(evt) {
+						storageEngine.delete('task', $(evt.target).data().taskId,
 							function() {
-								$(evt.target).parents('tr').remove(); 
+								$(evt.target).parents('tr').remove();
 								taskCountChanged();
 							}, errorLogger);
-						
+
 					}
 				);
-				
-				$(taskPage).find('#tblTasks tbody').on('click', '.editRow', 
-					function(evt) { 
+
+				$(taskPage).find('#tblTasks tbody').on('click', '.editRow',
+					function(evt) {
 						$(taskPage).find('#taskCreation').removeClass('not');
 						storageEngine.findById('task', $(evt.target).data().taskId, function(task) {
 							$(taskPage).find('form').fromObject(task);
 						}, errorLogger);
 					}
 				);
-				
+
 				$(taskPage).find('#clearTask').click(function(evt) {
 					evt.preventDefault();
 					clearTask();
 				});
-				
-				$(taskPage).find('#tblTasks tbody').on('click', '.completeRow', function(evt) { 					
+
+				$(taskPage).find('#tblTasks tbody').on('click', '.completeRow', function(evt) {
 					storageEngine.findById('task', $(evt.target).data().taskId, function(task) {
 						task.complete = true;
 						storageEngine.save('task', task, function() {
@@ -121,11 +132,11 @@ tasksController = function() {
 						},errorLogger);
 					}, errorLogger);
 				});
-				
+
 				$(taskPage).find('#saveTask').click(function(evt) {
 					evt.preventDefault();
 					if ($(taskPage).find('form').valid()) {
-						var task = $(taskPage).find('form').toObject();		
+						var task = $(taskPage).find('form').toObject();
 						storageEngine.save('task', task, function() {
 							$(taskPage).find('#tblTasks tbody').empty();
 							tasksController.loadTasks();
@@ -139,7 +150,10 @@ tasksController = function() {
 					evt.preventDefault();
 					$(taskPage).find('.teamWrapper').addClass('not');
 				});
-
+				$(taskPage).find('#userClose').click(function(evt) {
+					evt.preventDefault();
+					$(taskPage).find('.userWrapper').addClass('not');
+				});
 				$(taskPage).find('#saveTeam').click(function(event) {
 					event.preventDefault();
 					const teamForm = $(taskPage).find('#teamForm');
@@ -156,21 +170,21 @@ tasksController = function() {
 				initialised = true;
 			}
 		},
-        /**
+		/**
 		 * 111917kl
 		 * modification of the loadTasks method to load tasks retrieved from the server
-         */
+		 */
 		loadServerTasks: function(tasks) {
-            $(taskPage).find('#tblTasks tbody').empty();
-            $.each(tasks, function (index, task) {
-                if (!task.complete) {
-                    task.complete = false;
-                }
-                $('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
-                taskCountChanged();
-                console.log('about to render table with server tasks');
-                //renderTable(); --skip for now, this just sets style class for overdue tasks 111917kl
-            });
+			$(taskPage).find('#tblTasks tbody').empty();
+			$.each(tasks, function (index, task) {
+				if (!task.complete) {
+					task.complete = false;
+				}
+				$('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
+				taskCountChanged();
+				console.log('about to render table with server tasks');
+				//renderTable(); --skip for now, this just sets style class for overdue tasks 111917kl
+			});
 		},
 		loadTasks : function() {
 			$(taskPage).find('#tblTasks tbody').empty();
@@ -178,6 +192,7 @@ tasksController = function() {
 				tasks.sort(function(o1, o2) {
 					return Date.parse(o1.requiredBy).compareTo(Date.parse(o2.requiredBy));
 				});
+
 				$.each(tasks, function(index, task) {
 					if (!task.complete) {
 						task.complete = false;
@@ -187,6 +202,9 @@ tasksController = function() {
 					renderTable();
 				});
 			}, errorLogger);
-		} 
-	} 
+		}
+
+	}
+
+
 }();
